13e20fa0d836b79cc2212cf124adc9cb70c8a971
Dianettt <143523569+Dia-net@users.noreply.github.com>
2023-12-05 11:12:19 +0200

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,4 @@
 function getShippingMessage(country, price, deliveryFee) {
   return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
-getShippingMessage();
-console.log(getShippingMessage('Australia', 120, 50));
-console.log(getShippingMessage('Germany', 80, 20));
-console.log(getShippingMessage('Sweden', 100, 20));
+
